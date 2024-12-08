b4466e7a478e2daeae00d01c5daea4ccd7a863ad
royal0959 <70254342+royal0959@users.noreply.github.com>
2024-12-08 23:03:12 +0700

    feat: progress

---
+++ RailgunnerPassive/Asset.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.IO;
+using Plugin;
+
+public static class Asset
+{
+    public static AssetBundle mainBundle;
+    public const string bundleName = "passiveiconassetbundle";
+    public const string assetBundleFolder = "AssetBundles";
+
+    public static string AssetBundlePath
+    {
+        get
+        {
+            //return Path.Combine(Path.GetDirectoryName(Main.PInfo.Location), bundleName);
+            return Path.Combine(Path.GetDirectoryName(Main.PInfo.Location), assetBundleFolder, bundleName);
+        }
+    }
+
+    public static void Init()
+    {
+        mainBundle = AssetBundle.LoadFromFile(AssetBundlePath);
+        Log.Info(mainBundle.Contains("textAtomStabilizerIcon"));
+        Log.Info(mainBundle.Contains("textAtomStabilizerIcon.png"));
+        Log.Info(mainBundle.Contains("Assets/textAtomStabilizerIcon.png"));
+        Log.Info(mainBundle.Contains("Assets/textAtomStabilizerIcon"));
+
+
+    }
+}
--- RailgunnerPassive/Main.cs
+++ RailgunnerPassive/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using BepInEx;
 using R2API;
 using RoR2;
@@ -14,23 +15,67 @@ namespace Plugin
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     public class Main : BaseUnityPlugin
     {
+
+        public static PluginInfo PInfo { get; private set; }
+
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "royal";
         public const string PluginName = "RailgunnerCritChancePassive";
         public const string PluginVersion = "1.0.0";
 
+        private Sprite LoadSpriteFromDisk()
+        {
+            string path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location), "AtomStabilizer.png");
+            if (!File.Exists(path))
+            {
+                Log.Error("File not found at: " + path);
+                return null;
+            }
+
+            // Read the PNG file data into a byte array
+            byte[] imageData = File.ReadAllBytes(path);
+
+            // Create a new Texture2D (the size and format can be left empty since it will be overwritten)
+            Texture2D texture = new Texture2D(2, 2);
+
+            // Load the image data into the texture using ImageConversion.LoadImage
+            if (ImageConversion.LoadImage(texture, imageData))
+            {
+                // Create a Sprite from the loaded texture
+                Sprite newSprite = Sprite.Create(texture,
+                    new Rect(0, 0, texture.width, texture.height),
+                    new Vector2(0.5f, 0.5f));  // Pivot at the center
+
+                return newSprite;
+            }
+            else
+            {
+                Log.Error("Failed to load texture from image data.");
+                return null;
+            }
+        }
+
         public void Awake()
         {
+            Log.Init(Logger);
+
+            PInfo = Info;
+
+            Asset.Init();
+
             // https://risk-of-thunder.github.io/R2Wiki/Mod-Creation/Assets/Localization/
             GameObject railBodyPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/Railgunner/RailgunnerBody.prefab").WaitForCompletion();
 
-            LanguageAPI.Add("RAILGUNNER_PASSIVE_CRITCHANCE_NAME", "Placeholder");
+            LanguageAPI.Add("RAILGUNNER_PASSIVE_CRITCHANCE_NAME", "Atom Stabilizer");
             LanguageAPI.Add("RAILGUNNER_PASSIVE_CRITCHANCE_DESCRIPTION", $"Can <style=cIsDamage>Critically Strike</style>");
 
             PassiveItemSkillDef mySkillDef = ScriptableObject.CreateInstance<PassiveItemSkillDef>();
 
             mySkillDef.passiveItem = null;
-            mySkillDef.icon = Addressables.LoadAssetAsync<Sprite>("RoR2/DLC1/Railgunner/texCrosshairRailgunSniperCenter.png").WaitForCompletion();
+            //mySkillDef.icon = Addressables.LoadAssetAsync<Sprite>("RoR2/DLC1/Railgunner/texCrosshairRailgunSniperCenter.png").WaitForCompletion();
+            //mySkillDef.icon = Asset.mainBundle.LoadAsset<Sprite>("textAtomStabilizerIcon");
+            mySkillDef.icon = LoadSpriteFromDisk();
+
             mySkillDef.skillDescriptionToken = "RAILGUNNER_PASSIVE_CRITCHANCE_DESCRIPTION";
             mySkillDef.skillName = "RAILGUNNER_PASSIVE_CRITCHANCE_NAME";
             mySkillDef.skillNameToken = "RAILGUNNER_PASSIVE_CRITCHANCE_NAME";
